7498f58436324c3e64f33a8e8b3eeb63c9b1892f
Endi Merkuri <merkuriendi@gmail.com>
2024-06-02 04:24:03 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Junction Backend API')
+    .setTitle('Junction Backend API - Documentation')
     .setDescription('The Junction Backend API')
     .setVersion('1.0')
     .build();
